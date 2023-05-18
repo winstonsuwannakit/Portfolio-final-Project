ee5914a67cab877139479dbcd551715dbac66bf8
winstonsuwannakit <111588590+winstonsuwannakit@users.noreply.github.com>
2023-05-18 09:06:36 -0700

    Add files via upload

---
+++ transition.js
@@ -0,0 +1,148 @@
+function fadeOut1() {
+    const textBox2 = document.querySelector('#n2');
+    textBox2.style.opacity = '0';
+    textBox2.style['pointer-events'] = 'none';
+
+    const textBox3 = document.querySelector('#n3');
+    textBox3.style.opacity = '0';
+    textBox3.style['pointer-events'] = 'none';
+
+    const textBox1 = document.querySelector('#n1')
+    textBox1.style.opacity = '1';
+    textBox1.style['pointer-events'] = 'all';
+
+    const textBox4 = document.querySelector('#n4');
+    textBox4.style.opacity = '0';
+    textBox4.style['pointer-events'] = 'none';
+
+    const textBox5 = document.querySelector('#home');
+    textBox5.style.opacity = '0';
+    textBox5.style['pointer-events'] = 'none';
+
+    const textBox6 = document.querySelector('#about');
+    textBox6.style.opacity = '1';
+    textBox6.style['pointer-events'] = 'all';
+
+    const textBox7 = document.querySelector('#project')
+    textBox7.style.opacity = '1';
+    textBox7.style['pointer-events'] = 'all';
+
+    const textBox8 = document.querySelector('#contact');
+    textBox8.style.opacity = '1';
+    textBox8.style['pointer-events'] = 'all';
+}
+
+function fadeOut2() {
+    const textBox1 = document.querySelector('#n1');
+    textBox1.style.opacity = '0';
+    textBox1.style['pointer-events'] = 'none';
+
+    const textBox3 = document.querySelector('#n3');
+    textBox3.style.opacity = '0';
+    textBox3.style['pointer-events'] = 'none';
+
+    const textBox2 = document.querySelector('#n2')
+    textBox2.style.opacity = '1';
+    textBox2.style['pointer-events'] = 'all';
+
+    const textBox4 = document.querySelector('#n4');
+    textBox4.style.opacity = '0';
+    textBox4.style['pointer-events'] = 'none';
+
+    const textBox5 = document.querySelector('#home');
+    textBox5.style.opacity = '1';
+    textBox5.style['pointer-events'] = 'all';
+
+    const textBox6 = document.querySelector('#about');
+    textBox6.style.opacity = '0';
+    textBox6.style['pointer-events'] = 'none';
+
+    const textBox7 = document.querySelector('#project')
+    textBox7.style.opacity = '1';
+    textBox7.style['pointer-events'] = 'all';
+
+    const textBox8 = document.querySelector('#contact');
+    textBox8.style.opacity = '1';
+    textBox8.style['pointer-events'] = 'all';
+    
+}
+
+function fadeOut3() {
+    const textBox1 = document.querySelector('#n1');
+    textBox1.style.opacity = '0';
+    textBox1.style['pointer-events'] = 'none';
+
+    const textBox2 = document.querySelector('#n2')
+    textBox2.style.opacity = '0';
+    textBox2.style['pointer-events'] =  'none';
+
+    
+    const textBox3 = document.querySelector('#n3');
+    textBox3.style.opacity = '1';
+    textBox3.style['pointer-events'] = 'all';
+
+        
+    const textBox4 = document.querySelector('#n4');
+    textBox4.style.opacity = '0';
+    textBox4.style['pointer-events'] = 'none';
+
+    const textBox5 = document.querySelector('#home');
+    textBox5.style.opacity = '1';
+    textBox5.style['pointer-events'] = 'all';
+
+    const textBox6 = document.querySelector('#about');
+    textBox6.style.opacity = '1';
+    textBox6.style['pointer-events'] = 'all';
+
+    const textBox7 = document.querySelector('#project')
+    textBox7.style.opacity = '0';
+    textBox7.style['pointer-events'] = 'none';
+
+    const textBox8 = document.querySelector('#contact');
+    textBox8.style.opacity = '1';
+    textBox8.style['pointer-events'] = 'all';
+}
+
+
+function fadeOut4() {
+    const textBox1 = document.querySelector('#n1');
+    textBox1.style.opacity = '0';
+    textBox1.style['pointer-events'] = 'none';
+
+    const textBox2 = document.querySelector('#n2')
+    textBox2.style.opacity = '0';
+    textBox2.style['pointer-events'] =  'none';
+
+    
+    const textBox3 = document.querySelector('#n3');
+    textBox3.style.opacity = '0';
+    textBox3.style['pointer-events'] = 'none';
+
+        
+    const textBox4 = document.querySelector('#n4');
+    textBox4.style.opacity = '1';
+    textBox4.style['pointer-events'] = 'all';
+
+    const textBox5 = document.querySelector('#home');
+    textBox5.style.opacity = '1';
+    textBox5.style['pointer-events'] = 'all';
+
+    const textBox6 = document.querySelector('#about');
+    textBox6.style.opacity = '1';
+    textBox6.style['pointer-events'] = 'all';
+
+    const textBox7 = document.querySelector('#project')
+    textBox7.style.opacity = '1';
+    textBox7.style['pointer-events'] = 'all';
+
+    const textBox8 = document.querySelector('#contact');
+    textBox8.style.opacity = '0';
+    textBox8.style['pointer-events'] = 'none';
+}
+
+function kkkk() {
+    document.getElementById("#backgroundOK").style.backgroundImage = "url(wallpaper3.jpg)";
+ }
+
+
+
